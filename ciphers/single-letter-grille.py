a767df36e601d26d783755493a2460f5e21353d3
Drini Gashi <drini.gashi2@student.uni-pr.edu>
2026-04-12 23:54:47 +0200

    added main decrypt code for grille cipher

--- ciphers/single-letter-grille.py
+++ ciphers/single-letter-grille.py
@@ -11,10 +11,37 @@ def single_letter_grille_encrypt(plainText: str) -> str:
     pass
 
 def single_letter_grille_decrypt(cipherText: str) -> str:
-    pass
+    holes = grille_positions()
+
+    cipherText = cipherText.replace("\n", "")
+    block_size = 36
+
+    plainText = ""
+
+    for b in range(0, len(cipherText), block_size):
+        block = cipherText[b:b+block_size]
+
+        grid = {}
+        k = 0
+
+        for i in range(6):
+            for j in range(6):
+                grid[(i,j)] = block[k]
+                k += 1
+
+        for (i,j) in holes:
+            plainText += grid[(i,j)]
+
+    return plainText.rstrip('X')
 
 def main():
-    pass
+    text = input("Shkruaj tekstin: ")
+
+    encrypted = single_letter_grille_encrypt(text)
+    print(f"\nEncrypted:\n{encrypted}")
+
+    decrypted = single_letter_grille_decrypt(encrypted)
+    print(f"Decrypted: {decrypted}")
 
 if __name__ == "__main__":
     main()
