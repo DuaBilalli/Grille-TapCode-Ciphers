eb280b3d8bfb943aad6453d6ea6577f88b4d608b
Drini Gashi <drini.gashi2@student.uni-pr.edu>
2026-04-12 23:49:53 +0200

    added grille positions

--- ciphers/single-letter-grille.py
+++ ciphers/single-letter-grille.py
@@ -1,3 +1,13 @@
+import string
+
+def grille_positions() -> list:
+    #pozitat per vrimat e lira
+    return [
+        (0,1), (1,4), (2,2),
+        (2,3), (3,0), (3,2),
+        (4,5), (5,1), (5,3)
+    ]
+
 def single_letter_grille_encrypt(plainText: str) -> str:
     pass
 
