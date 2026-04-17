4541838312c3316dde8a6c0f8aa8c0e686f3e278
eda5krq <eda.krasniqi2@student.uni-pr.edu>
2026-04-18 01:39:24 +0200

    implement letter position finder

--- ciphers/tap_code.py
+++ ciphers/tap_code.py
@@ -7,3 +7,21 @@ TAP_MATRIX = [
     ['Q', 'R', 'S', 'T', 'U'],
     ['V', 'W', 'X', 'Y', 'Z']
 ]
+def find_position(letter):
+    if letter == 'K':
+        letter = 'C'
+
+    row_number = 1
+
+    for row in TAP_MATRIX:
+        col_number = 1
+
+        for char in row:
+            if char == letter:
+                return row_number, col_number
+            col_number += 1
+
+        row_number += 1
+
+    return None
+
