cc09528e056d15ccfdf6803add2844e68d40a433
eda5krq <eda.krasniqi2@student.uni-pr.edu>
2026-04-18 20:26:14 +0200

    Improved main.py

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from ciphers.tap_code import encrypt_tap_code
 text = input("Shkruaj tekstin: ")
 encrypted_text = encrypt_tap_code(text)
 
+print()
 print("Teksti i enkriptuar:")
 print(encrypted_text)
+print()
