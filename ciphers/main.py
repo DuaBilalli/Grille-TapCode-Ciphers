5872f1bd00c2ca57042bb808aa30774c48070477
eda5krq <eda.krasniqi2@student.uni-pr.edu>
2026-04-18 23:56:10 +0200

    Modified main.py

--- ciphers/main.py
+++ ciphers/main.py
@@ -1,10 +1,14 @@
-from single_letter_grille import single_letter_grille_encrypt, single_letter_grille_decrypt, visualize_grille_process
+from ciphers.single_letter_grille import single_letter_grille_encrypt, single_letter_grille_decrypt, visualize_grille_process
+from ciphers.tap_code import encrypt_tap_code, visualize_tapcode_process
+
 
 def main():
     while True:
         print("\nZgjedhnni algoritmin që dëshironi të përdorni:\n")
         print("1. Single-Letter Grille")
         print("2. Tap Code")
+        print("3. Dil")
+
         choice = input("\nZgjedh algoritmin që dëshironi të përdorni: ")
 
         if choice == "1":
@@ -14,12 +18,29 @@ def main():
             print(f"\nEncrypted:\n{cipherText}")
 
             decrypted = single_letter_grille_decrypt(cipherText)
-            print(f"\nDecrypted: {plainText}")
+            print(f"\nDecrypted:\n{decrypted}")
 
             show = input("\nA dëshironi të shihni vizualizimin? (y/n): ").lower()
             if show == "y":
                 visualize_grille_process(cipherText)
 
-                
-if __name__=="__main__":
+        elif choice == "2":
+            plainText = input("\nShkruaj plaintext-in: ")
+
+            cipherText = encrypt_tap_code(plainText)
+            print(f"\nEncrypted:\n{cipherText}")
+
+            show = input("\nA dëshironi të shihni vizualizimin? (y/n): ").lower()
+            if show == "y":
+                visualize_tapcode_process(plainText)
+
+        elif choice == "3":
+            print("\nProgrami u mbyll.")
+            break
+
+        else:
+            print("\nZgjedhje e pavlefshme, provo përsëri.")
+
+
+if __name__ == "__main__":
     main()
--- main.py
+++
@@ -1,9 +0,0 @@
-from ciphers.tap_code import encrypt_tap_code
-
-text = input("Shkruaj tekstin: ")
-encrypted_text = encrypt_tap_code(text)
-
-print()
-print("Teksti i enkriptuar:")
-print(encrypted_text)
-print()
